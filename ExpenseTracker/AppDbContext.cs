d07d14cd49419edf6b6de0b50fc2032499d16984
Gustavo Malisse <gostavomalisse@gmail.com>
2025-03-19 22:32:20 -0300

    Created the docker compose file, created the DbContext and injected it in the Program

---
+++ ExpenseTracker/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryType> CategoryTypes { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+
+}
--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace ExpenseTracker
 {
     public class Program
@@ -13,6 +15,10 @@ namespace ExpenseTracker
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(connectionString));
 
             var app = builder.Build();
 
