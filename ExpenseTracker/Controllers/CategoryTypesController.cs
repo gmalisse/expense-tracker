995a86b7832734c9ca1d826a9ac6542f842d72b6
Gustavo Malisse <gostavomalisse@gmail.com>
2025-03-20 17:18:56 -0300

    Add CategoryTypesController with basic CRUD operations

---
+++ ExpenseTracker/Controllers/CategoryTypesController.cs
@@ -0,0 +1,50 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryTypesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryTypesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CategoryType>> Get()
+        {
+            try
+            {
+                var categoryTypes = _context.CategoryTypes.ToList();
+                return categoryTypes;
+            }
+            catch(Exception)
+            {
+                return NotFound("There are no categories.");
+            }
+            
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<CategoryType> Get(int id)
+        {
+            try
+            {
+                var categoryType = _context.CategoryTypes.
+                    SingleOrDefault(c => c.Id == id);
+                return categoryType;
+            }
+            catch (Exception)
+            {
+                return NotFound("Category not found.");
+            }
+        }
+    }
+}
