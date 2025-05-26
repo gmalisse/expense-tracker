6aaf21b59be6156de0780242c1da6f8846d98729
Gustavo Malisse <gostavomalisse@gmail.com>
2025-05-26 20:57:38 -0300

    update: new crud operations on transactions controller

---
+++ ExpenseTracker/Controllers/TransactionsController.cs
@@ -0,0 +1,49 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TransactionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Transaction>> Get()
+        {
+            try
+            {
+                var transactions = _context.Transactions.ToList();
+                return transactions;
+            }
+            catch (Exception)
+            {
+                return NotFound("No transactions found.");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Transaction> Post(Transaction transaction)
+        {
+            try
+            {
+                _context.Transactions.Add(transaction);
+                _context.SaveChanges();
+                return CreatedAtAction(nameof(Get), new { id = transaction.Id }, transaction);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creating category.");
+            }
+        }
+    }
+}
